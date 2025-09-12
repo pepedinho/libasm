0fbd09ad5af40433e0408d85b0119bba18542c30
pepedinho <2spii94@gmail.com>
2025-09-12 17:43:19 +0200

    func: improve ft_atoi_base implementation

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -6,27 +6,72 @@ extern ft_strlen
 section .text:
   ft_atoi_base:
     push  rbx
+    xor   rbx, rbx
+    mov   cl, 0    ; neg = 0
+
     test  rdi, rdi ; check str == NULL
     jz    .error
     test  rsi, rsi ; check base == NULL
     jz    .error
-
-    mov   bl, [rdi] ;lis le premier caractere
     
   .loop_on_sign:
     mov   al, [rdi] ; charger le caractere dans al
-    cmp   al, '-'
-    je    .next_char ; on passe au caractere suivant
-    cmp   al, '+'
-    jne    .loop ; is != '+' '-' on sort de la boucle
+    test  al, al    ; *str == \0 ?
+    jz    .end      ;  si oui, fin
 
-  .next_char:
-    inc [rdi]
+    cmp   al, '-'
+    jne   .check_plus ; on verifie si on est sur un '+'
+    mov   cl, 1       ; si == '-' neg = 1
+    inc   rdi
+    jne   .loop_on_sign ; if != '+' '-' on sort de la boucle
+
+
+  .check_plus:
+    cmp,  al '+'
+    jne   .loop   ; si != '+' break
+    inc   rdi
     jmp   .loop_on_sign
 
+
   .loop:
-    test [rdi], [rdi]
-    jz
+    mov   al, [rdi]
+    test  al, al
+    jz    .end
+  
+    push  rdi       ; on stock rdi dans la stack
+    
+    mov   rdi, rsi  ; on stock rsi (base) dans rdi pour le passer a ft_strlen
+    call  ft_strlen ; rax = strlen(base)
+    
+    pop   rdi       ; on restore rdi
+    imul  ebx, eax  ; res *= ft_strlen(base)
+    
+    cmp   al, '0'
+    jl    .error    ; si < 0 il ne peux appartenir a aucune autre base donc error (jl == jump if less than 0)
+    cmp   al, '9'
+    jg    .alpha    ; si > 0 il peux appartenir a alpha ou caps_alpha donc on check (jg = jump if greater than 0)
+    
+    jmp   .num
+
+  .num:
+    ; TODO
+    jmp   .next
+  
+  .alpha:
+    ; TODO
+    jmp   .next
+
+  .caps_alpha:
+    ; TODO
+    jmp   .next
+
+  .next: 
+    inc   rdi
+    jmp   .loop
+
+  .end:
+    pop rbx
+    ; TODO
 
   .error:
     mov rax, 0 ; return 0 
